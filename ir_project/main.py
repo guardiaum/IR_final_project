053513c559f519c809bb354ef19c90349701ba58
Johny Moreira <jms5@cin.ufpe.br>
2017-11-26 13:48:56 -0300

    wikipedia crawler page content

--- ir_project/main.py
+++ ir_project/main.py
@@ -1,18 +1,28 @@
 import json
 import requests
 
-pages = ['George_Orwell', 'Neil_Gaiman']
+pages = ['George_Orwell|Neil_Gaiman|George_R._R._Martin|Dan_Brown|Thomas_Bulfinch|Ted_Chiang|Margaret_Atwood|Stephen_King|Doris_Dana|Peter_Ackroyd|John_Boswell|Jamie_O%27Neill']
 
 baseurl = 'http://en.wikipedia.org/w/api.php'
 my_atts = {}
 my_atts['action'] = 'query'
 my_atts['prop'] = 'revisions'
 my_atts['rvprop'] = 'content'
+#my_atts['rvsection'] = '0'
 my_atts['format'] = 'json'
+my_atts['titles'] = pages
+
+resp = requests.get(baseurl, params=my_atts)
+
+pages = resp.json()
+pages = (pages.get('query')).get('pages')
 
 for page in pages:
-    my_atts['titles'] = page
-    resp = requests.get(baseurl, params=my_atts)
-    data = resp.json()
-    print(data['query']['pages'])
-    #print(data['query'][0]['revisions'][0]['*'])
+
+    #title = (pages[page]).get('title')
+    #print(title)
+    revisions = (pages[page]).get('revisions')
+    content = revisions[0].get("*")
+
+    print(content)
+    print('==========================================================================================')
