ed36927e48cdc37a446e5db628402d920300718f
Johny Moreira <jms5@cin.ufpe.br>
2017-11-27 19:18:30 -0300

    break document in sentences

--- ir_project/main.py
+++ ir_project/main.py
@@ -1,8 +1,10 @@
 import re
+import io
+import os
 import requests
 import mwparserfromhell
 
-pages = ['George_Orwell|Douglas_Adams|Ted_Chiang|Neil_Gaiman|George_R._R._Martin|Dan_Brown|Thomas_Bulfinch|J._K._Rowling|Dante_Alighieri|Margaret_Atwood|Stephen_King|Doris_Dana|Peter_Ackroyd|John_Boswell']
+pages = ['George_Orwell|Douglas_Adams|Ted_Chiang|Neil_Gaiman|Doris_Dana|Peter_Ackroyd|John_Boswell|George_R._R._Martin|Dan_Brown|Thomas_Bulfinch|J._K._Rowling|Dante_Alighieri|Margaret_Atwood|Stephen_King']
 
 baseurl = 'http://en.wikipedia.org/w/api.php'
 my_atts = {}
@@ -28,10 +30,28 @@ for page in pages:
     revisions = (pages[page]).get('revisions')
     content = revisions[0].get("*")
     parsed_wikicode = mwparserfromhell.parse(content)
+    content = parsed_wikicode
+
     content = parsed_wikicode.strip_code()
 
-    file = open("corpus/"+title,'w')
-    content = content.encode('utf-8')
+    file = io.open("corpus/"+title+"_OLD", "w", encoding="utf-8")
     file.write(content)
-    file.close()
+
+    fileNew = io.open("corpus/" + title, "w", encoding="utf-8")
+
+    lines = io.open("corpus/"+title+"_OLD", 'r', encoding="utf-8")
+
+    for line in lines:
+        if not line.strip(): continue
+        fileNew.write(line)
+        '''sentences = re.split(r'(?<!\s\w)\.\s*', line)
+        print(sentences)
+        for sentence in sentences:
+            if not sentence.strip(): continue
+            print(sentence)
+            fileNew.write(sentence + "\n")'''
+    fileNew.close()
+
+    os.remove("corpus/"+title+"_OLD")
+
     print('==========================================================================================')
--- ir_project/nltk_test.py
+++ ir_project/nltk_test.py
@@ -7,16 +7,18 @@ for file in os.listdir("corpus"):
     lines = io.open("corpus/" + file, 'r', encoding="utf-8")
 
     for line in lines:
+        print(line)
+        # sentence segmenter
         sentence = nltk.sent_tokenize(line)
 
+        # word tokenizer
         sentence = [nltk.word_tokenize(sent) for sent in sentence]
-
+        # apply part-of-speech tagger
         sentence = [nltk.pos_tag(sent) for sent in sentence]
 
-        grammar = "NP: {<DT>?<JJ>*<NN>}"
-        print sentence
+        grammar = r"""NP:{<DT|PP\$>?<JJ>*<NN>}"""
 
         cp = nltk.RegexpParser(grammar)
-        result = cp.parse(sentence)
+        result = cp.parse(sentence[0])
 
         print result
